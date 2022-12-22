11b2f61c4033f5e8e8dce1e13ce6153e38202f8f
Steven Markham <doltishmite@gmail.com>
2022-12-22 13:56:06 +0000

    Setting up application layout, adding chart, tracking values and control buttons. Setting up anchroing via SplitContainer Controls so that the application can be resized.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,14 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.MainChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.WinButton = new System.Windows.Forms.Button();
             this.LossButton = new System.Windows.Forms.Button();
+            this.WinLossRatioLabel = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.StatTrackTitleLabel = new System.Windows.Forms.Label();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.WinsCounterLabel = new System.Windows.Forms.Label();
+            this.DrawsCounterLabel = new System.Windows.Forms.Label();
+            this.LossesCounterLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.MainChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainChart
@@ -43,40 +59,42 @@
             this.MainChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea4.AxisX.Title = "Match Number";
-            chartArea4.AxisY.Interval = 10D;
-            chartArea4.AxisY.Maximum = 100D;
-            chartArea4.AxisY.Minimum = 0D;
-            chartArea4.AxisY.Title = "Percentage";
-            chartArea4.Name = "ChartArea1";
-            this.MainChart.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.MainChart.Legends.Add(legend4);
-            this.MainChart.Location = new System.Drawing.Point(0, -1);
+            chartArea1.AxisX.Minimum = 1D;
+            chartArea1.AxisX.Title = "Match Number";
+            chartArea1.AxisY.Interval = 10D;
+            chartArea1.AxisY.Maximum = 100D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.Title = "Percentage";
+            chartArea1.Name = "ChartArea1";
+            this.MainChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.MainChart.Legends.Add(legend1);
+            this.MainChart.Location = new System.Drawing.Point(3, 3);
             this.MainChart.Name = "MainChart";
-            series4.BorderWidth = 3;
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Color = System.Drawing.Color.Black;
-            series4.Legend = "Legend1";
-            series4.Name = "Win / Loss Ratio";
-            this.MainChart.Series.Add(series4);
-            this.MainChart.Size = new System.Drawing.Size(1228, 479);
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Black;
+            series1.Legend = "Legend1";
+            series1.Name = "Win / Loss Ratio";
+            this.MainChart.Series.Add(series1);
+            this.MainChart.Size = new System.Drawing.Size(874, 502);
             this.MainChart.TabIndex = 0;
             this.MainChart.Text = "chart1";
-            title4.Name = "Title1";
-            title4.Text = "Win / Loss Ratio Chart";
-            this.MainChart.Titles.Add(title4);
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            title1.Name = "Title1";
+            title1.Text = "Win Rate Percentage Chart";
+            this.MainChart.Titles.Add(title1);
             // 
             // WinButton
             // 
             this.WinButton.AutoSize = true;
             this.WinButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.WinButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.WinButton.Dock = System.Windows.Forms.DockStyle.Top;
             this.WinButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.WinButton.Location = new System.Drawing.Point(0, 517);
+            this.WinButton.Location = new System.Drawing.Point(0, 0);
             this.WinButton.Name = "WinButton";
-            this.WinButton.Size = new System.Drawing.Size(1228, 41);
+            this.WinButton.Size = new System.Drawing.Size(170, 41);
             this.WinButton.TabIndex = 2;
             this.WinButton.Text = "Win";
             this.WinButton.UseVisualStyleBackColor = true;
@@ -88,28 +106,137 @@
             this.LossButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.LossButton.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.LossButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.LossButton.Location = new System.Drawing.Point(0, 476);
+            this.LossButton.Location = new System.Drawing.Point(0, 211);
             this.LossButton.Name = "LossButton";
-            this.LossButton.Size = new System.Drawing.Size(1228, 41);
+            this.LossButton.Size = new System.Drawing.Size(170, 41);
             this.LossButton.TabIndex = 3;
             this.LossButton.Text = "Loss";
             this.LossButton.UseVisualStyleBackColor = true;
             this.LossButton.Click += new System.EventHandler(this.LossButton_Click);
             // 
+            // WinLossRatioLabel
+            // 
+            this.WinLossRatioLabel.AutoSize = true;
+            this.WinLossRatioLabel.Location = new System.Drawing.Point(8, 34);
+            this.WinLossRatioLabel.Name = "WinLossRatioLabel";
+            this.WinLossRatioLabel.Size = new System.Drawing.Size(119, 13);
+            this.WinLossRatioLabel.TabIndex = 4;
+            this.WinLossRatioLabel.Text = "Win / Loss Ration: N/A";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.MainChart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1054, 511);
+            this.splitContainer1.SplitterDistance = 880;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.LossesCounterLabel);
+            this.splitContainer2.Panel1.Controls.Add(this.DrawsCounterLabel);
+            this.splitContainer2.Panel1.Controls.Add(this.WinsCounterLabel);
+            this.splitContainer2.Panel1.Controls.Add(this.StatTrackTitleLabel);
+            this.splitContainer2.Panel1.Controls.Add(this.WinLossRatioLabel);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.DrawButton);
+            this.splitContainer2.Panel2.Controls.Add(this.LossButton);
+            this.splitContainer2.Panel2.Controls.Add(this.WinButton);
+            this.splitContainer2.Size = new System.Drawing.Size(170, 511);
+            this.splitContainer2.SplitterDistance = 255;
+            this.splitContainer2.TabIndex = 6;
+            // 
+            // StatTrackTitleLabel
+            // 
+            this.StatTrackTitleLabel.AutoSize = true;
+            this.StatTrackTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.StatTrackTitleLabel.Location = new System.Drawing.Point(6, 9);
+            this.StatTrackTitleLabel.Name = "StatTrackTitleLabel";
+            this.StatTrackTitleLabel.Size = new System.Drawing.Size(119, 25);
+            this.StatTrackTitleLabel.TabIndex = 5;
+            this.StatTrackTitleLabel.Text = "Stat Tracker";
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.AutoSize = true;
+            this.DrawButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.DrawButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DrawButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.DrawButton.Location = new System.Drawing.Point(0, 41);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(170, 170);
+            this.DrawButton.TabIndex = 4;
+            this.DrawButton.Text = "Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // WinsCounterLabel
+            // 
+            this.WinsCounterLabel.AutoSize = true;
+            this.WinsCounterLabel.Location = new System.Drawing.Point(8, 47);
+            this.WinsCounterLabel.Name = "WinsCounterLabel";
+            this.WinsCounterLabel.Size = new System.Drawing.Size(60, 13);
+            this.WinsCounterLabel.TabIndex = 6;
+            this.WinsCounterLabel.Text = "Wins : N/A";
+            // 
+            // DrawsCounterLabel
+            // 
+            this.DrawsCounterLabel.AutoSize = true;
+            this.DrawsCounterLabel.Location = new System.Drawing.Point(8, 60);
+            this.DrawsCounterLabel.Name = "DrawsCounterLabel";
+            this.DrawsCounterLabel.Size = new System.Drawing.Size(66, 13);
+            this.DrawsCounterLabel.TabIndex = 7;
+            this.DrawsCounterLabel.Text = "Draws : N/A";
+            // 
+            // LossesCounterLabel
+            // 
+            this.LossesCounterLabel.AutoSize = true;
+            this.LossesCounterLabel.Location = new System.Drawing.Point(8, 73);
+            this.LossesCounterLabel.Name = "LossesCounterLabel";
+            this.LossesCounterLabel.Size = new System.Drawing.Size(69, 13);
+            this.LossesCounterLabel.TabIndex = 8;
+            this.LossesCounterLabel.Text = "Losses : N/A";
+            // 
             // MainWindowForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1228, 558);
-            this.Controls.Add(this.LossButton);
-            this.Controls.Add(this.WinButton);
-            this.Controls.Add(this.MainChart);
+            this.ClientSize = new System.Drawing.Size(1054, 511);
+            this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimumSize = new System.Drawing.Size(871, 297);
             this.Name = "MainWindowForm";
             this.Text = "Win Rate Projector";
             ((System.ComponentModel.ISupportInitialize)(this.MainChart)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -118,6 +245,14 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart MainChart;
         private System.Windows.Forms.Button WinButton;
         private System.Windows.Forms.Button LossButton;
+        private System.Windows.Forms.Label WinLossRatioLabel;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.Label StatTrackTitleLabel;
+        private System.Windows.Forms.Label LossesCounterLabel;
+        private System.Windows.Forms.Label DrawsCounterLabel;
+        private System.Windows.Forms.Label WinsCounterLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,41 +12,75 @@ namespace Win_Rate_Projector
 {
     public partial class MainWindowForm : Form
     {
+        double MatchWinsTracker;
+        double MatchLossesTracker;
+        double MatchDrawsTracker;
+        int MatchCounter;
+
         public MainWindowForm()
         {
             InitializeComponent();
         }
 
-        double MatchCounter;
-
-        double MatchWinsTracker;
-        double MatchLossesTracker;
-
-        //Total Wins/Total Matches * 100 = Percentage won
-
-
         private void WinButton_Click(object sender, EventArgs e)
         {
-            MatchCounter = MatchCounter + 1;
-            MatchWinsTracker = MatchWinsTracker + 1;
-            
+            MatchCounter++;
+            MatchWinsTracker++;
+
+            WinsCounterLabel.Text = ("Wins: " + MatchWinsTracker.ToString());
+
+
             MainChart.Series["Win / Loss Ratio"].Points.AddXY(MatchCounter, (MatchWinsTracker / MatchCounter) * 100);
 
+            WinLossRatioLabel.Text = "Win / Loss Ratio: " + Math.Round((MatchWinsTracker/MatchCounter), 2);
+
+
+            if (Math.Round((MatchWinsTracker / MatchCounter), 2) >= 0.55)
+            {
+                MainChart.Series["Win / Loss Ratio"].Color = Color.Green;
+            }
+            else if (Math.Round((MatchWinsTracker / MatchCounter), 2) <= 0.45)
+            {
+                MainChart.Series["Win / Loss Ratio"].Color = Color.Red;
+            }
+            else if (Math.Round((MatchWinsTracker / MatchCounter), 2) > 0.45 && (Math.Round((MatchWinsTracker / MatchCounter), 2) < 0.55))
+            {
+                MainChart.Series["Win / Loss Ratio"].Color = Color.Black;
+            }
+
+            MainChart.Series["Win / Loss Ratio"].Color= Color.Green;
             
             Console.WriteLine("DEBUG FOR ROUND " + MatchCounter);
             Console.WriteLine("Ratio Calculator: " + (MatchWinsTracker / MatchCounter) * 100);
             Console.WriteLine("Wins Tracker: " + MatchWinsTracker);
             Console.WriteLine("Losses Tracker: " + MatchLossesTracker);
-            Console.WriteLine("Match Counter: " + MatchCounter);
             Console.WriteLine();
+
         }
 
         private void LossButton_Click(object sender, EventArgs e)
         {
-            MatchCounter = MatchCounter + 1;
-            MatchLossesTracker = MatchLossesTracker + 1;
-           
+            MatchCounter++;
+            MatchLossesTracker++;
+
+            LossesCounterLabel.Text = ("Losses: " + MatchLossesTracker.ToString());
+
             MainChart.Series["Win / Loss Ratio"].Points.AddXY(MatchCounter, (MatchWinsTracker / MatchCounter) * 100);
+
+            if (Math.Round((MatchWinsTracker / MatchCounter), 2) >= 0.55)
+            {
+                MainChart.Series["Win / Loss Ratio"].Color = Color.Green;
+            }
+            else if (Math.Round((MatchWinsTracker / MatchCounter), 2) <= 0.45)
+            {
+                MainChart.Series["Win / Loss Ratio"].Color = Color.Red;
+            }
+            else if (Math.Round((MatchWinsTracker / MatchCounter), 2) > 0.45 && (Math.Round((MatchWinsTracker / MatchCounter), 2) < 0.55))
+            {
+                MainChart.Series["Win / Loss Ratio"].Color = Color.Black;
+            }
+
+            WinLossRatioLabel.Text = "Win / Loss Ratio: " + Math.Round((MatchWinsTracker / MatchCounter), 2);
 
 
             Console.WriteLine("DEBUG FOR ROUND " + MatchCounter);
@@ -56,5 +90,26 @@ namespace Win_Rate_Projector
             Console.WriteLine("Match Counter: " + MatchCounter);
             Console.WriteLine();
         }
+
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            MatchCounter++;
+            MatchDrawsTracker++;
+
+            DrawsCounterLabel.Text = ("Draws: " + MatchDrawsTracker.ToString());
+            
+            MainChart.Series["Win / Loss Ratio"].Points.AddXY(MatchCounter, (MatchWinsTracker / MatchCounter) * 100);
+
+            WinLossRatioLabel.Text = "Win / Loss Ratio: " + Math.Round((MatchWinsTracker / MatchCounter), 2);
+
+            Console.WriteLine("DEBUG FOR ROUND " + MatchCounter);
+            Console.WriteLine("Ratio Calculator: " + (MatchWinsTracker / MatchCounter) * 100);
+            Console.WriteLine("Wins Tracker: " + MatchWinsTracker);
+            Console.WriteLine("Losses Tracker: " + MatchLossesTracker);
+            Console.WriteLine("Match Counter: " + MatchCounter);
+            Console.WriteLine();
+        }
+
     }
 }
